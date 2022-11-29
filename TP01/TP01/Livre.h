cebf845beea56282171713a7398f05960716abdc
Malhuile <maloric_martinez@etu.u-bourgogne.fr>
2022-11-29 15:16:33 +0100

    Création class Lecteur, maj class Livre

--- TP01/TP01/Livre.h
+++ TP01/TP01/Livre.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+#include "Date.h"
+
+class Livre
+{
+public:
+	Livre();
+	
+	enum Genres {
+		Science_fiction, Aventure, Biographie, Historique, Realiste, Education, Romance 
+	};
+
+
+private:
+	std::string tire;
+	std::string auteur;
+	std::string langue;
+	Genres genre;
+	Date date;
+	int ISBN;
+};
