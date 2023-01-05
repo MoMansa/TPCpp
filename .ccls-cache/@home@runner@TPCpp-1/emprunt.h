cfe915b18aa8aceb9f955308746e57a0608675d4
MoMansa <118825296+MoMansa@users.noreply.github.com>
2023-01-05 09:23:34 +0000

    livre non complet 3

---
+++ .ccls-cache/@home@runner@TPCpp-1/emprunt.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "date.h"
+#include "livre.h"
+#include "lecteur.h"
+
+#ifndef EMPRUNT_H
+#define EMPRUNT_H
+
+class Emprunt {
+    public:
+        Emprunt(Date empruntdate, Date retourdate, Livre livre, Lecteur lecteur);
+        std::string toString();
+
+    private:
+        Date _empruntdate;
+        Date _retourdate;
+        int _isbn;
+        std::string _id;
+};
+
+#endif
